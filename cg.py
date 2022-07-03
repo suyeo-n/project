8b0176079554da797c72be0d90123c933a729e07
suyeon <tnduswhddls@naver.com>
2022-07-03 12:07:36 +0900

    first commit

---
+++ cg.py
@@ -0,0 +1,27 @@
+seqs = {}
+seq = ''
+seqid = ''
+
+def GC(seq):
+    gc = (seqs[seqid].count('G')+seqs[seqid].count('C'))/len(seq)*100
+    return gc
+
+for line in open('rosalind_gc.txt', 'r'):
+  print(line)
+  if line[0] == '>':
+    if len(seq) > 0:
+      seqs[seqid] = seq
+      '''seqs[seqid] = GC(seq)'''
+      
+    seqid = line.rstrip()[1:]
+    seq = ''
+  else:
+    seq += line.rstrip().upper()
+
+if len(seq) > 0:
+    seqs[seqid] = seq
+    '''seqs[seqid] = GC(seq)'''
+print(seqs)
+maxid = max(seqs, key=seqs.get)
+
+print(maxid, seqs[maxid])
